3634047b0af682045b790ede11914148f139f774
Mason Katz <mason.katz@gmail.com>
2008-10-18 00:56:13 +0000

    copyright 5.1

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
 #
-# $Id: Makefile,v 1.17 2008/08/06 21:06:57 bruno Exp $
+# $Id: Makefile,v 1.18 2008/10/18 00:56:13 mjk Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		            version 5.0 (V)
+# 		           version 5.1  (VI)
 # 
 # Copyright (c) 2000 - 2008 The Regents of the University of California.
 # All rights reserved.	
@@ -55,6 +55,9 @@
 # @Copyright@
 #
 # $Log: Makefile,v $
+# Revision 1.18  2008/10/18 00:56:13  mjk
+# copyright 5.1
+#
 # Revision 1.17  2008/08/06 21:06:57  bruno
 # fixes to build OS roll
 #
--- graph.py
+++ graph.py
@@ -1,12 +1,12 @@
 #!/opt/rocks/bin/python
 #
-# $Id: graph.py,v 1.11 2008/03/06 23:41:56 mjk Exp $
+# $Id: graph.py,v 1.12 2008/10/18 00:56:13 mjk Exp $
 #
 # @Copyright@
 # 
 # 				Rocks(r)
 # 		         www.rocksclusters.org
-# 		            version 5.0 (V)
+# 		           version 5.1  (VI)
 # 
 # Copyright (c) 2000 - 2008 The Regents of the University of California.
 # All rights reserved.	
@@ -56,6 +56,9 @@
 # @Copyright@
 #
 # $Log: graph.py,v $
+# Revision 1.12  2008/10/18 00:56:13  mjk
+# copyright 5.1
+#
 # Revision 1.11  2008/03/06 23:41:56  mjk
 # copyright storm on
 #
